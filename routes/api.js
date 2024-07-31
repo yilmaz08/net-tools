d7b4a2bc5c3c67ea1adc391ebe82e4152a7834af
Abdürrahim YILMAZ <84680978+yilmaz08@users.noreply.github.com>
2024-07-31 03:04:22 +0300

    chore: init
    Generated with express-generator and configured as API
    
    API has:
    
    /api/port - Open Port Checker
    /api/dns - DNS Lookup
    /api/rdns - Reverse DNS Lookup

---
+++ routes/api.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var router = express.Router();
+const net = require('net');
+const dns = require('dns');
+
+function check_port(host, port, timeout) {
+  return new Promise((resolve) => {
+    const socket = new net.Socket();
+    let isOpen = false;
+
+    if (isNaN(port)) { return false; }
+
+    socket.setTimeout(timeout);
+
+    socket.on('connect', () => { isOpen = true; socket.destroy(); });
+    socket.on('timeout', () => { isOpen = false; socket.destroy(); });
+    socket.on('error', (err) => { isOpen = false; socket.destroy(); });
+
+    socket.on('close', () => { console.log("lesgoo: " + port); resolve(isOpen); });
+
+    socket.connect(port, host);
+  });
+}
+function dns_lookup(domain, type) {
+  return new Promise((resolve) => {
+    dns.resolve(domain, type, (err, addresses) => {
+      if (err) {
+        if (err.code === 'ENOTFOUND' || err.code === "ENODATA") { resolve([]); }
+        else { resolve(err); }
+      }
+      else { resolve(addresses); }
+    });
+  });
+}
+function rdns_lookup(ip) {
+  return new Promise((resolve) => {
+    dns.reverse(ip, (err, hostnames) => {
+      if (err) { resolve([]); }
+      else { resolve(hostnames); }
+    });
+  });
+}
+
+// --- ROUTES ---
+
+// Open Port Check
+router.post('/port', async function(req, res) {
+  let port = req.query.port;
+  let host = req.query.host;
+  if (!port) { res.status(400).json({message: 'Port is required'}); return; }
+  if (!host) { res.status(400).json({message: 'Host is required'}); return; }
+
+  results = {};
+
+  let ports = port.split(',').map(p => parseInt(p)); // split ports and convert to int
+  ports = ports.filter((p, i) => ports.indexOf(p) === i); // remove duplicates
+
+  let portChecks = ports.map(async (p) => { 
+    try { results[p] = await check_port(host, p, 1000); }
+    catch (err) { results[p] = false; }
+  });
+
+  await Promise.all(portChecks);
+
+  console.log(results);
+  res.json(results);
+});
+
+// DNS lookup
+router.post('/dns', async function(req, res) {
+  let domain = req.query.domain;
+  let types = req.query.types;
+  if (!domain) { res.status(400).json({message: 'Domain is required'}); return; }
+  if (!types) { types = "A,AAAA,CNAME,TXT,MX"; }
+
+  let typesArr = types.split(',').map(t => t.toUpperCase());
+  results = {};
+
+  let dnsChecks = typesArr.map(async (t) => {
+    try { results[t] = await dns_lookup(domain, t); }
+    catch (err) { results[t] = []; }
+  });
+
+  await Promise.all(dnsChecks);
+
+  console.log(results);
+  res.json(results);
+});
+
+// Reverse DNS lookup
+router.post('/rdns', async function(req, res) {
+  let ip = req.query.ip;
+  if (!ip) { res.status(400).json({message: 'IP is required'}); return; }
+
+  try { result = await rdns_lookup(ip); }
+  catch { result = null; }
+
+  res.json(result);
+});
+
+module.exports = router;
